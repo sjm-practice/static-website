9752612c25ef9dadf653b626b71b1eb4f31b16d9
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-04-23 17:47:46 -0400

    complete storybook setup, add NavBar stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,17 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import React, { Fragment } from "react";
+import { GlobalStyle } from "../src/globalStyle";
+
+function withGlobalStyle(storyFn) {
+  return (
+    <Fragment>
+      <GlobalStyle />
+      {storyFn()}
+    </Fragment>
+  );
+}
+
+addDecorator(withGlobalStyle);
 
 const req = require.context("../src", true, /__stories__\/.*.stories.jsx?/);
 
--- src/components/NavBar/__tests__/__stories__/NavBar.stories.js
+++ src/components/NavBar/__tests__/__stories__/NavBar.stories.js
@@ -1,13 +1,5 @@
-// import React from "react";
-// import { storiesOf } from "@storybook/react";
-// import { Button } from "@storybook/react/demo";
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import NavBar from "../../index";
 
-// storiesOf("Button", module)
-//   .add("with text", () => <Button>Hello Button</Button>)
-//   .add("with emoji", () => (
-//     <Button>
-//       <span role="img" aria-label="so cool">
-//         😀 😎 👍 💯
-//       </span>
-//     </Button>
-//   ));
+storiesOf("Components", module).add("NavBar", () => <NavBar />);
