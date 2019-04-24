6124b3e63becceb56f205c0fce0e7965dfb808b7
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-04-24 16:08:14 -0400

    add react-test-renderer and first snapshot test

--- src/components/NavBar/__tests__/NavBar.test.js
+++ src/components/NavBar/__tests__/NavBar.test.js
@@ -1,7 +1,11 @@
 /* eslint-env jest */
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import NavBar from "../index";
 
 describe("<NavBar />", () => {
   it("should render and match snapshot", () => {
-    expect(1).toBe(1);
+    const testRenderer = TestRenderer.create(<NavBar />);
+    expect(testRenderer.toJSON()).toMatchSnapshot();
   });
 });
