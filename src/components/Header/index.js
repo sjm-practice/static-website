8a15369a958b043f22edb7f229244ce52b82345f
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-04-25 17:48:05 -0400

    adjust Header placeholder component appearance

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
 
-const Header = () => {
-  return <div id="Header">Header</div>;
+const Header = ({ classes }) => {
+  return (
+    <div id="Header" className={classes.container}>
+      <Typography variant="h5">Header</Typography>
+    </div>
+  );
 };
 
-export default Header;
+export default withStyles(styles)(Header);
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,7 +2,8 @@ const styles = () => ({
   container: {
     width: "100%",
     height: "300px",
-    background: "darkgreen",
+    padding: "10px",
+    background: "green",
   },
 });
 
