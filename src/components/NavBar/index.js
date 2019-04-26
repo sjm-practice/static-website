d12ebc44e7ead28b47a7f137a2f13221daf6660b
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2019-04-26 14:14:35 -0400

    remove unused items from appbar, add logo / image

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,25 +1,19 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "@reach/router";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
-import InputBase from "@material-ui/core/InputBase";
 import Badge from "@material-ui/core/Badge";
 import MenuItem from "@material-ui/core/MenuItem";
 import Menu from "@material-ui/core/Menu";
 import { withStyles } from "@material-ui/core/styles";
-import MenuIcon from "@material-ui/icons/Menu";
-import SearchIcon from "@material-ui/icons/Search";
 import AccountCircle from "@material-ui/icons/AccountCircle";
 import MailIcon from "@material-ui/icons/Mail";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import MoreIcon from "@material-ui/icons/MoreVert";
 import styles from "./styles";
-
-// import { Link } from "@reach/router";
-//       <Link to="/">Home</Link>
-//       <a href="https://avoinsights.com/dashboard/index.php">Dashboard</a>
 
 class NavBar extends Component {
   state = {
@@ -100,48 +94,13 @@ class NavBar extends Component {
       <div className={classes.root}>
         <AppBar position="fixed">
           <Toolbar>
-            <IconButton
-              className={classes.menuButton}
-              color="inherit"
-              aria-label="Open drawer"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography className={classes.title} variant="h6" color="inherit" noWrap>
-              Material-UI
-            </Typography>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search…"
-                classes={{
-                  root: classes.inputRoot,
-                  input: classes.inputInput,
-                }}
-              />
-            </div>
+            <Link to="/">
+              <img className={classes.logo} src="images/avoLogo.png" alt="Logo" />
+            </Link>
             <div className={classes.grow} />
             <div className={classes.sectionDesktop}>
-              <IconButton color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                  <MailIcon />
-                </Badge>
-              </IconButton>
-              <IconButton color="inherit">
-                <Badge badgeContent={17} color="secondary">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
-              <IconButton
-                aria-owns={isMenuOpen ? "material-appbar" : undefined}
-                aria-haspopup="true"
-                onClick={this.handleProfileMenuOpen}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
+              <a href="#Features">Features</a>
+              <a href="https://avoinsights.com/dashboard/index.php">Dashboard</a>
             </div>
             <div className={classes.sectionMobile}>
               <IconButton
